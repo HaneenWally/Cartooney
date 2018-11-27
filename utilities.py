9b4210c1a8c6e1f373197c7bdfaf76c8313c1bf5
HaneenWally <megemen2020@gmail.com>
2018-11-27 19:39:04 +0200

    Add utilities
    This class encloses common and basic functionalities that are needed as intermediate calculations in some algorithms.

---
+++ utilities.py
@@ -0,0 +1,22 @@
+Class Utilities:
+
+  def __init__(self, image):
+    
+    self.__img = image
+    return
+  
+  #calculates histogram
+  def hist(self):
+    return
+  
+  #calculates cumulative histogram
+  def cumHist(self):
+    return
+  
+  #converts color image to grey
+  def grey(self):
+    return
+   
+  #corrects image values (hint: when values are negative, exponential, float, ..etc
+  def norm(self):
+    return
