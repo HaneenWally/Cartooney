70c7b6d166e1a1aa2759089051f7f78b25a7c7e3
HaneenWally <megemen2020@gmail.com>
2018-11-27 19:43:07 +0200

    Update edge.py

--- edge.py
+++ edge.py
@@ -4,9 +4,11 @@ Class Edge:
   
     self.__img = image
     return
-    
+  
+  #apply canny to find edges
   def canny(self):
     return
-    
+   
+  #apply sharpening to get enhanced visual effect of edges
   def sharpening(self):
     return
