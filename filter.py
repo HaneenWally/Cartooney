37cf4a2845089c383450a32067ccbc31a488ddea
HaneenWally <megemen2020@gmail.com>
2018-11-27 19:46:58 +0200

    Add filter class
    This class holds the masks and effects that are added to detected face/s.

---
+++ filter.py
@@ -0,0 +1,14 @@
+Class Filter:
+
+  def __init__(self, image):
+    
+    self.__img = image
+    return
+    
+  #apply mustache filter to detected face
+  def mustache(self):
+    return
+  
+  #apply hat filter to detected face
+  def hat(self):
+    return
