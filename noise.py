9ec991d5a0ac75f0d2d91dd41c0e3df60f18694a
HaneenWally <megemen2020@gmail.com>
2018-11-27 19:41:48 +0200

    Update noise.py

--- noise.py
+++ noise.py
@@ -4,9 +4,11 @@ Class Noise:
     
     self.__image = img
     return
-   
+  
+  #apply median filter if image has impulsive nois
   def medianFilter(self):
     return
-    
+  
+  #apply gaussian filter when image has additive noise
   def Gaussian(self):
     return
