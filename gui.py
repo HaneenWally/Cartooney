870b5e620741c8fdf0e982da2ce6229955f90bf8
HaneenWally <megemen2020@gmail.com>
2018-11-28 07:48:35 +0200

    Add gun class
    The name is descriptive enough ;D

---
+++ gui.py
@@ -0,0 +1,7 @@
+class GUI:
+
+  def __init__(self):
+    return
+    
+  #TODO implement seperate functions for button creation and handling, label creation and handling, 
+  #     image showing, input taking, option menu(filter options), ..etc
